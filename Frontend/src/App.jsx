1981cd8b70182beaeb48a66fcb385a49cea735e8
MANNE UDAY KIRAN <udaykiran7615124@gmail.com>
2025-06-24 16:27:29 +0530

    third commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,8 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 // import Chat from './pages/Chat'; // Optional
 import Chat from './components/Chat'; // Importing Chat component
-import { Navigate } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, IconButton } from '@mui/material';
 import { lightTheme, darkTheme } from './theme';
-import { Brightness4, Brightness7 } from '@mui/icons-material';
 import React, { useState } from 'react';
 
 
@@ -15,14 +12,8 @@ function App() {
   const [darkMode, setDarkMode] = useState(false);
 
 
-  const toggleDark = () => setDarkMode((prev) => !prev);
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      {/* <CssBaseline />
-      <IconButton onClick={toggleDark} sx={{ position: 'absolute', top: 8, right: 800 }}>
-        {darkMode ? <Brightness7 /> : <Brightness4 />}
-      </IconButton> */}
-      {/* Your routes/components here */}
+      
       <Router>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -32,7 +23,6 @@ function App() {
 
       </Routes>
     </Router>
-    </ThemeProvider>
   );
 }
 
