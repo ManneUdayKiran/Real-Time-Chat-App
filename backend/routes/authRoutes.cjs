a73e4ef7670013d0a9ea6664b1e84d0948681039
MANNE UDAY KIRAN <143863214+ManneUdayKiran@users.noreply.github.com>
2025-06-24 16:16:35 +0530

    Update authRoutes.cjs

--- backend/routes/authRoutes.cjs
+++ backend/routes/authRoutes.cjs
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController.cjs');
 // const authMiddleware = require('../middlewares/authMiddleware.cjs');
-const authMiddleware = require('../middlewares/authMiddleWare.cjs'); // 👈 Use the correct path
+const authMiddleware = require('../middlewares/authMiddleware.cjs'); // 
 
 router.post('/register', authController.register);
-router.post('/login', authController.login); // 👈 Add this
-
+router.post('/login', authController.login); // 
 router.get('/profile', authMiddleware, (req, res) => {
   res.json({ message: 'You are authenticated', user: req.user });
 });
